1a6ace0c749d1f076a336d62b88b8eebe0e156e1
Jim Fowler <fowler@math.osu.edu>
2021-07-07 21:10:28 -0400

    include logging

--- src/index.ts
+++ src/index.ts
@@ -2,28 +2,27 @@ import express, { Express, Request, Response } from 'express';
 import bodyParser from 'body-parser';
 import helmet from 'helmet';
 import dotenv from 'dotenv';
+import getAuthorize from './authorize';
+import postToken from './token';
+import Logger from "./logger";
+import morganMiddleware from './morgan-middleware';
 
 dotenv.config();
 
 const PORT = process.env.PORT || 3000;
 const app: Express = express();
 
+app.use(morganMiddleware);
+
 app.use(helmet());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get('/authorize', (req: Request, res: Response) => {
-  res.send('<h1>Hello from the TypeScript world!</h1>');
+app.get('/authorize', getAuthorize);
+
+app.post('/token', postToken);
+
+app.listen(PORT, () => {
+  Logger.debug(`Server is up and running @ http://localhost:${PORT}`);
 });
-
-app.post('/token', (req: Request, res: Response) => {
-  res.send('<h1>Hello from the TypeScript world!</h1>');
-});
-
-
-app.get('/', (req: Request, res: Response) => {
-  res.send('<h1>Hello from the TypeScript world!</h1>');
-});
-
-app.listen(PORT, () => console.log(`Running on ${PORT} ⚡`));
 
--- src/token.ts
+++ src/token.ts
@@ -1,5 +1,5 @@
 import { Request, Response } from 'express';
 
-export function token(req: Request, res: Response) {
-  res.send('<h1>Hello from the TypeScript world!</h1>');
+export default function post(req: Request, res: Response) {
+  res.send('<h1>Hello from the post world!</h1>');
 }
